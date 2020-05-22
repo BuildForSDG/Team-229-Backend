9742fe0a0e2959c65828dade8df42c34c27cba79
ssensalo <edgaarayhan@gmail.com>
2020-05-22 21:10:52 +0300

    Initial dashboard interface

---
+++ eduFormDashBoard/database/migrations/2020_05_22_060504_create_collectors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCollectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('collectors', function (Blueprint $table) {
+            $table->id();
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('contact');
+            $table->string('address_one');
+            $table->string('address_two');
+            $table->string('email');
+            $table->string('pass1');
+            $table->string('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('collectors');
+    }
+}
